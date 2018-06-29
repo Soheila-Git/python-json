6d36fd0a7445dd23b39bfd1c5df2440fb8d5790e
Soheila Akbarzadeh <soheila.akbarzadeh@gmail.com>
2018-06-29 14:57:15 -0700

    add comments to show the output

--- python-json.py
+++ python-json.py
@@ -1,3 +1,9 @@
+'''
+Based on Youtube video:
+Python Tutorial: Working with JSON Data using the json Module
+By Corey Schafer
+https://www.youtube.com/watch?v=9N6a-VLBa2I
+'''
 import json
 
 people_string = '''
@@ -19,5 +25,13 @@ people_string = '''
 }
 '''
 
+# Load JSON to Python object
 data = json.loads(people_string)
+
 print(data)
+'''
+{'people': [{'name': 'John Smith', 'phone': '616-555-7187', 'emails':
+['johnsmith@gmail.com', 'jsmith@work.com'], 'has_license': False},
+{'name': 'Jane Doe', 'phone': '543-453-5544', 'emails': None,
+'has_license': True}]}
+'''
